07f83d568e09c6048903e0e9282c153c2bfa9deb
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-04 16:09:53 +0300

    Fix formatting by adding missing newlines at the end of sales and sale_items migration files

--- database/migrations/2026_05_04_092839_create_sales_table.php
+++ database/migrations/2026_05_04_092839_create_sales_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
                 'cash',
                 'mpesa',
                 'bank',
-                'card'
+                'card',
             ]);
 
             $table->foreignId('user_id')
