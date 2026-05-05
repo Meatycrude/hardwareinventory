995f69cea90ea11e5a8824e1dee42c6accc9363e
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-05 16:12:54 +0300

    Add SaleFactory for generating sale model instances

---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\sale;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<sale>
+ */
+class SaleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'invoice_number' => $this->faker->unique()->numerify('INV-#####'),
+            'total_amount' => $this->faker->numberBetween(10, 99),
+            'payment_method' => $this->faker->randomElement(['cash', 'mpesa', 'bank', 'card']),
+            'user_id' => null, // You can set this to a valid user ID if needed
+     
+        ];
+    }
+}
