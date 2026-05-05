64ca123c9e7b4f90e43c807fdc9da31ce95c1c77
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-05 16:13:43 +0300

    Add SupplierFactory for generating supplier model instances

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\supplier;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<supplier>
+ */
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'email' => $this->faker->unique()->companyEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+          
+        ];
+    }
+}
