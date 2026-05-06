3701be12aaaf2065d1b3c0b93621418686ca97c1
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-06 15:27:53 +0300

    Fix formatting in api.php by adding missing newlines for better readability

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/products', [ProductController::class, 'store']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/{product}', [ProductController::class, 'show']);
+
+Route::put('/products/{product}', [ProductController::class, 'update']);
