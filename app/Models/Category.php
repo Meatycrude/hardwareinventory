d7d93c6f0091b016d6dda01aed55298729d737b8
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-06 13:53:07 +0300

    jfhhdfiugifgui

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,16 +3,21 @@
 namespace App\Models;
 
 use Database\Factories\CategoryFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['name', 'slug'])]
 class Category extends Model
 {
     /** @use HasFactory<CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
 
     protected static function boot()
     {
@@ -23,32 +28,46 @@ class Category extends Model
         });
 
         static::updating(function ($category) {
+
             if ($category->isDirty('name')) {
                 $category->generateSlug();
             }
+
         });
     }
 
-    public function generateSlug()
+    public function generateSlug(): void
     {
         $slug = Str::slug($this->name);
+
         $originalSlug = $slug;
+
         $counter = 1;
 
         $query = self::where('slug', $slug);
+
         if ($this->id) {
             $query->where('id', '!=', $this->id);
         }
 
         while ($query->exists()) {
-            $slug = $originalSlug.'-'.$counter;
+
+            $slug = $originalSlug . '-' . $counter;
+
             $counter++;
+
             $query = self::where('slug', $slug);
+
             if ($this->id) {
                 $query->where('id', '!=', $this->id);
             }
         }
 
         $this->slug = $slug;
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
     }
 }
