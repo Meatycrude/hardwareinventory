1dd1ba9a5126a99c53c89d757dfe62d9f51de189
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-07 17:12:29 +0300

    Add sales routes for managing sales operations

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\SaleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,11 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 
 Route::put('/products/{product}', [ProductController::class, 'update']);
+
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+Route::post('/sales', [SaleController::class, 'store']);
+
+Route::get('/sales', [SaleController::class, 'index']);
+
+Route::get('/sales/{sale}', [SaleController::class, 'show']);
