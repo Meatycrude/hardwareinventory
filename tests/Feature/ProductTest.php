2f790693a5af673f763cd8327ac5c65b0a39993c
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-12 09:14:40 +0300

    Remove unused Product model import from ProductTest for cleaner code

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,14 +2,10 @@
 
 namespace Tests\Feature;
 
-use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
     use RefreshDatabase;
-
-    
-
 }
