7d5f0e62f422d96f44086a5e676aaddbcf5ae69c
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-12 09:13:05 +0300

    Remove unused imports in ProductResource.php

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -4,8 +4,6 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Http\Resources\CategoryResource;
-use App\Http\Resources\SupplierResource;
 
 class ProductResource extends JsonResource
 {
