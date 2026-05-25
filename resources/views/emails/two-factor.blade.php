c850fd3b8b6b4d3b569f06c332d1b3a0f5fb1900
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-25 14:00:08 +0300

    Fix capitalization in two-factor email template

--- resources/views/emails/two-factor.blade.php
+++ resources/views/emails/two-factor.blade.php
@@ -1,5 +1,5 @@
 <h2>kaura hardware </h2>
-<p>your login verification code is :</p>
+<p>Your login verification code is :</p>
 <h1>{{ $code }}</h1>
-<p>this code expires in 10 minutes </p>
+<p>This code expires in 10 minutes </p>
 <p>If you didn't request this code please ignore this email </p>
