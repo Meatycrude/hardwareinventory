6714bb4ab5201eb4d410ca223925a56cae9fc8c8
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-25 12:26:52 +0300

    blade template for the gmail account

---
+++ resources/views/emails/two-factor.blade.php
@@ -0,0 +1,5 @@
+<h2>kaura hardware </h2>
+<p>your login verification code is :</p>
+<h1>{{ $code }}</h1>
+<p>this code expires in 10 minutes </p>
+<p>If you didn't request this code please ignore this email </p>
