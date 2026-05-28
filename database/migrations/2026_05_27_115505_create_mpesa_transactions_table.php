0dcb872f6b91134f9b2cb8f26a8ef6daecc81098
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-28 16:59:17 +0300

    deleted a migration  for movement types

--- database/migrations/2026_05_27_115505_create_mpesa_transactions_table.php
+++ database/migrations/2026_05_27_115505_create_mpesa_transactions_table.php
@@ -12,17 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('mpesa_transactions', function (Blueprint $table) {
-    $table->id();
-    $table->string('checkout_request_id')->unique();
-    $table->string('merchant_request_id')->nullable();
-    $table->string('phone');
-    $table->decimal('amount', 12, 2);
-    $table->string('status')->default('pending');
-    $table->string('mpesa_receipt_number')->nullable();
-    $table->json('cart_items')->nullable();
-    $table->json('callback_payload')->nullable();
-    $table->timestamps();
-});
+            $table->id();
+            $table->string('checkout_request_id')->unique();
+            $table->string('merchant_request_id')->nullable();
+            $table->string('phone');
+            $table->decimal('amount', 12, 2);
+            $table->string('status')->default('pending');
+            $table->string('mpesa_receipt_number')->nullable();
+            $table->json('cart_items')->nullable();
+            $table->json('callback_payload')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
