9134b50c126c1a5108251ff2f3c532b084fecf99
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-05-28 16:57:36 +0300

    mpesa transaction

--- app/Models/MpesaTransaction.php
+++ app/Models/MpesaTransaction.php
@@ -2,27 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-#[fillable(['checkout_request_id',
+#[Fillable(['checkout_request_id',
     'merchant_request_id',
     'phone',
     'amount',
     'status',
     'mpesa_receipt_number',
     'cart_items',
-    'callback_payload',])]
+    'callback_payload', ])]
 
 class MpesaTransaction extends Model
 {
-    
-
-protected $casts = [
-    'cart_items' => 'array',
-    'callback_payload' => 'array',
-];
+    protected $casts = [
+        'cart_items' => 'array',
+        'callback_payload' => 'array',
+    ];
     //
 }
